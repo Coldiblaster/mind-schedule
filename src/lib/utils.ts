d8b1346dbb027726ffc25e5b28584c8f387fdfdb
coldiblaster <vbastazin@gmail.com>
2024-09-30 16:00:03 -0300

    feat: add getInitials

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getInitials(fullName: string | undefined): string {
+  if (!fullName) return '';
+
+  // Divida o nome completo em partes e pegue a primeira letra de cada palavra
+  const nameParts = fullName.split(' ');
+  const initials = nameParts.map(part => part.charAt(0).toUpperCase());
+
+  // Limite a quantidade de iniciais a 2 (primeiro e segundo nome, por exemplo)
+  return initials.slice(0, 2).join('');
+}
