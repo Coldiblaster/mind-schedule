4b4fd63c5fb6f6502466012aea780f7bc5624ee5
coldiblaster <vbastazin@gmail.com>
2024-09-30 16:00:31 -0300

    feat: melhorando responsividade

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import { Separator } from './ui/separator';
 export function Header() {
   return (
     <div className="border-b">
-      <div className="flex h-20 items-center gap-6 px-6">
+      <div className="flex h-20 items-center gap-4 px-6 md:gap-6">
         <Brain className="h-6 w-6" />
 
         <Separator orientation="vertical" className="h-8" />
