f7b971c6026d66b2d5fbc43962d5d6607c363ed9
coldiblaster <vbastazin@gmail.com>
2024-10-07 10:43:46 -0300

    feat: add icon

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,18 +11,18 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, icon, ...props }, ref) => {
     return (
-      <div>
+      <div className="relative flex items-center">
         <input
           type={type}
           className={cn(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            `${icon ? 'pl-3 pr-8' : 'px-3'} flex h-10 w-full rounded-md border border-input bg-background py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
             className,
           )}
           ref={ref}
           {...props}
         />
 
-        {icon && <Icon name={icon} />}
+        {icon && <Icon className="absolute right-3" name={icon} />}
       </div>
     );
   },
