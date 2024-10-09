65a24889859226a402678dcc7ae8ec44f35512f0
coldiblaster <vbastazin@gmail.com>
2024-10-09 00:45:36 -0300

    feat: criando pagina de teste

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,9 +1,21 @@
 'use client';
 
-import { useClerk } from '@clerk/nextjs';
+import { DashboardPatient } from '@/containers/dashboard/patient';
+import { DashboardProfessional } from '@/containers/dashboard/professional';
+import { useAuth } from '@/hooks/use-auth';
+import { UserTypes } from '@/types/user-types';
 
 export default function Home() {
-  const { user } = useClerk();
+  const { isLoading, userType } = useAuth();
 
-  return <div>Dashboard - {user?.id}</div>;
+  if (isLoading) {
+    return <h1>Carregando...</h1>;
+  }
+
+  const DashboardComponent =
+    userType === UserTypes.PROFESSIONAL
+      ? DashboardProfessional
+      : DashboardPatient;
+
+  return <DashboardComponent />;
 }
---
+++ src/containers/dashboard/patient/index.tsx
@@ -0,0 +1,3 @@
+export const DashboardPatient = () => {
+  return <h1>Dash Patient</h1>;
+};
