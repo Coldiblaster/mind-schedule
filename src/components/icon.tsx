de094a545ccadb92791796be1ef71275d1bd597b
coldiblaster <vbastazin@gmail.com>
2024-10-15 15:20:40 -0300

    feat: add react-icons/pi

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { type IconBaseProps } from 'react-icons/lib';
 import * as LuIcon from 'react-icons/lu';
 import * as MdIcon from 'react-icons/md';
+import * as PiIcon from 'react-icons/pi';
 import { match, P } from 'ts-pattern';
 
-export type IconName = keyof typeof MdIcon | keyof typeof LuIcon;
+export type IconName =
+  | keyof typeof MdIcon
+  | keyof typeof LuIcon
+  | keyof typeof PiIcon;
 
 export type IconProps = IconBaseProps & {
   name: IconName;
@@ -14,6 +18,7 @@ export function Icon({ name, ...props }: IconProps) {
   const element = match(name)
     .with(P.string.startsWith('Md'), icon => MdIcon[icon])
     .with(P.string.startsWith('Lu'), icon => LuIcon[icon])
+    .with(P.string.startsWith('Pi'), icon => PiIcon[icon])
     .otherwise(() => React.Fragment);
 
   return React.createElement(element, props);
