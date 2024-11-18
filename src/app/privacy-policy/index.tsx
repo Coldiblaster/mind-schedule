116b5a1898116a9ea6b3a87bff81219b68e68e21
coldiblaster <vbastazin@gmail.com>
2024-11-17 21:06:25 -0300

    feat: add pagina de politica de privacidade

---
+++ src/app/privacy-policy/index.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <h1 className="text-2xl md:text-5xl">
+        Política de privacidade da Mind Schedule
+      </h1>
+    </div>
+  );
+}
