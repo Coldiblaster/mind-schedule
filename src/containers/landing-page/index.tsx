6b208ae7df12b1531281c10b52e8b89de8e73c24
coldiblaster <vbastazin@gmail.com>
2024-11-19 12:07:23 -0300

    feat: aplicando animações, melhorias de textos e responsividade

--- src/containers/landing-page/index.tsx
+++ src/containers/landing-page/index.tsx
@@ -1,61 +1,66 @@
+import { useInView } from 'framer-motion';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 
 import { CompanyLogo } from '@/components/company-logo';
 import { Caption, Display2 } from '@/components/typography';
-import { Button } from '@/components/ui/button';
 
 export const LandingPage = () => {
-  const router = useRouter();
-  const goToRegister = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    router.push('/?register=true');
-  };
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+
+  const isInView1 = useInView(ref1, { once: true, amount: 0.5 });
+  const isInView2 = useInView(ref2, { once: true, amount: 0.5 });
+  const isInView3 = useInView(ref3, { once: true, amount: 0.5 });
+
   return (
     <main
       id="landing-page"
       className="relative flex w-full animate-fade flex-col items-center pb-32"
     >
-      <div className="relative flex h-[calc(100vh+170px)] w-full flex-col items-center justify-center">
+      <div className="relative flex h-[calc(100vh+170px)] w-full flex-col">
         <div className="to-dark absolute top-0 h-full w-full bg-primary bg-gradient-to-b from-primary clip-polygon-clip dark:bg-primary">
           <div className="absolute -left-[550px] -top-[550px] h-[960px] w-[960px] rounded-full bg-gradient-balls-top-left" />
           <div className="absolute -right-[450px] -top-[300px] h-[1024px] w-[1024px] rounded-full bg-gradient-balls-top-right" />
           <div className="absolute -bottom-[550px] -left-[550px] h-[960px] w-[960px] rounded-full bg-gradient-balls-bottom-left" />
         </div>
 
-        <section className="absolute top-0 flex min-h-screen flex-col items-center justify-center text-white">
-          <div className="mt-auto flex flex-col items-center">
-            <div className="flex flex-col items-center gap-3 text-lg">
-              <CompanyLogo width={140} height={140} />
-              <Caption as="100" className="mt-5 text-center">
-                Mind Schedule
-              </Caption>
-            </div>
-            <div className="mt-[10vh] flex flex-col items-center gap-4">
-              <span>Saiba mais</span>
-              <div className="relative flex h-9 w-6 animate-pulse items-center justify-center rounded-3xl border-2 border-white animate-duration-1000">
-                <div className="absolute top-1 h-2 w-1 animate-bounce rounded-3xl bg-white animate-duration-1000">
-                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white opacity-65" />
-                </div>
-              </div>
-            </div>
+        <section className="relative z-10 flex min-h-svh flex-col items-center justify-center gap-10 text-white md:gap-20">
+          <div className="flex animate-fade flex-col items-center gap-3 animate-delay-300">
+            <CompanyLogo width={140} height={140} />
+            <Caption as="100" className="mt-5 text-center text-5xl md:text-7xl">
+              Mind Schedule
+            </Caption>
           </div>
-          <div className="flex w-full flex-col px-4 py-11 md:max-w-[600px]">
-            <Display2 as="700">Agende fácil seu dia!</Display2>
+          <div className="flex w-full animate-fade-up flex-col px-4 text-center animate-delay-500 animate-duration-500 md:max-w-[600px]">
+            <h1 className="text-2xl font-bold md:text-3xl">
+              Simplifique seu tempo, potencialize seus resultados
+            </h1>
             <p className="mt-4 text-sm lg:text-lg">
               Transforme sua rotina e conquiste mais clientes!
-              <br />
-              <br />
-              Gerencie agendamentos com eficiência, otimize atendimentos e
-              aumente a satisfação dos seus clientes.
             </p>
+            <p className="text-sm md:mt-2 lg:text-lg">
+              Revolucione sua gestão de tempo e transforme cada agendamento em
+              uma oportunidade de crescimento para seu negócio.
+            </p>
+          </div>
+          <div className="bottom-10 flex flex-col items-center gap-4 md:absolute">
+            <span>Saiba mais</span>
+            <div className="relative flex h-9 w-6 animate-pulse items-center justify-center rounded-3xl border-2 border-white">
+              <div className="absolute top-1 h-2 w-1 animate-bounce rounded-3xl bg-white animate-duration-1000">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white opacity-65" />
+              </div>
+            </div>
           </div>
         </section>
       </div>
 
-      <section className="relative -mt-32 w-full">
-        <div className="mx-auto flex flex-col gap-12 md:flex-row md:items-center">
-          <div className="w-72 animate-fade-right lg:w-1/2">
+      <section ref={ref1} className="relative -mt-32 w-full">
+        <div className="mx-auto flex animate-fade-down flex-col gap-12 md:flex-row md:items-center">
+          <div
+            className={`${isInView1 ? 'w-72 animate-fade-right animate-delay-200 lg:w-1/2' : 'hidden'}`}
+          >
             <Image
               src="/home/image-left.svg"
               alt="Gestão"
@@ -64,30 +69,38 @@ export const LandingPage = () => {
               height={362}
             />
           </div>
-          <div className="w-full p-4 lg:max-w-[330px]">
+          <div
+            className={`${isInView1 ? 'w-full animate-fade-down p-4 animate-delay-700 lg:max-w-[330px]' : 'hidden'}`}
+          >
             <Display2 as="700" className="text-foreground">
               Facilite os agendamentos!
             </Display2>
             <p className="mt-4 text-sm text-muted-foreground lg:text-lg">
-              Facilite a vida dos seus clientes com agendamentos online e
-              personalizados.
+              Nossa plataforma inteligente automatiza seus agendamentos, elimina
+              conflitos de horários e oferece uma experiência excepcional para
+              seus clientes, permitindo que você foque no que realmente importa:
+              o sucesso do seu negócio.
             </p>
           </div>
         </div>
       </section>
 
-      <section className="relative z-10 mt-16 lg:mt-32">
+      <section ref={ref2} className="relative z-10 mt-16 lg:mt-32">
         <div className="mx-auto flex flex-col-reverse items-center gap-12 md:flex-row">
-          <div className="p-4 text-right lg:w-1/2">
+          <div
+            className={`${isInView2 ? 'animate-fade-right p-4 text-right animate-delay-300 lg:w-1/2' : 'hidden'}`}
+          >
             <Display2 as="700" className="text-foreground">
               Agilidade e eficiência!
             </Display2>
             <p className="mt-4 text-sm text-muted-foreground lg:text-lg">
-              Com o cliente agendando online, você tem mais tempo para focar no
-              que realmente importa: o atendimento.
+              Gerencie agendamentos com eficiência, otimize atendimentos e
+              aumente a satisfação dos seus clientes.
             </p>
           </div>
-          <div className="w-72 animate-fade-left self-end lg:w-1/2">
+          <div
+            className={`${isInView2 ? 'w-72 animate-fade-left self-end animate-delay-500 lg:w-1/2' : 'hidden'}`}
+          >
             <Image
               src="home/image-right.svg"
               alt="Eficiência"
@@ -99,16 +112,18 @@ export const LandingPage = () => {
         </div>
       </section>
 
-      <section className="relative">
+      <section className="relative" ref={ref3}>
         <Image
           src="home/rocket.svg"
           alt="Rocket"
-          className="-z-10 mx-auto mb-8 h-auto w-screen animate-fade-up xl:-mt-[140px]"
+          className={`${isInView3 ? '-z-10 mx-auto mb-8 h-auto w-screen animate-fade-up xl:-mt-[140px]' : 'hidden'} `}
           width={1000}
           height={1000}
         />
 
-        <div className="relative mx-auto flex h-full w-full flex-col items-center gap-6 p-4">
+        <div
+          className={`${isInView3 ? 'relative mx-auto flex h-full w-full animate-fade-up flex-col items-center gap-6 p-4 animate-delay-[2000ms]' : 'hidden'}`}
+        >
           <Display2 as="700" className="text-foreground">
             Vamos voar juntos! 🚀
           </Display2>
@@ -116,11 +131,9 @@ export const LandingPage = () => {
             Com a Mind Schedule, você tem tudo o que precisa para decolar e
             conquistar o sucesso!
           </p>
-          <div className="mt-8">
-            <Button size="lg" onClick={() => goToRegister()}>
-              Criar conta grátis
-            </Button>
-          </div>
+          {/* <div className="mt-8">
+            <Button size="lg">Criar conta grátis</Button>
+          </div> */}
         </div>
       </section>
     </main>
